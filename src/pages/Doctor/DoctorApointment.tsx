5703373376e51eff2197edd4ee21b14385e633fb
LENOVO <anjanaheshan676@gmail.com>
2025-11-14 13:28:58 +0530

    doctor Login feature added

---
+++ src/pages/Doctor/DoctorApointment.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DoctorApointment = () => {
+  return (
+    <div>DoctorApointment</div>
+  )
+}
+
+export default DoctorApointment
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { DoctorContext } from '../context/DoctorContext'
 
 
 const Login = () => {
@@ -12,6 +13,7 @@ const Login = () => {
     const [password , setPassword] = useState('')
     
     const {setAToken,backendUrl} = useContext(AdminContext)
+    const {setDToken} = useContext(DoctorContext)
 
     const onSubmitHandler = async (e) => {
         e.preventDefault()
@@ -33,6 +35,17 @@ const Login = () => {
 
             }else {
 
+                const {data} = await axios.post(`${backendUrl}/api/v1/doctor/login` , {email , password})
+
+                if (data.success) {
+                    console.log(data.token)
+                    localStorage.setItem('dToken',data.token)
+                    toast.success('Doctor Loged Successfull ...')
+                    setDToken(data.token)
+                    console.log(data.token)
+                } else {
+                    toast.error(data.message)
+                }
 
             }
 
